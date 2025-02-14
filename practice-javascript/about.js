6c81cc4f41a3f4ead408bdd34dad986725063026
kainatfatima1605 <kainatrafiq1625@gmail.com>
2025-02-14 14:16:46 -0800

    loop

---
+++ practice-javascript/about.js
@@ -0,0 +1,104 @@
+///main question
+
+//  var num = [10, 20, 30, 40, 50];
+//  var add = num.push(70,80) //// push method add the element
+// var remove = num.pop(1)/////// pop method remove the last array
+  // var addNew = num.unshift(0,11,12)///// unshift method add  a value in the start of element
+  //var copy = num.slice(4)///// coopy the element
+  //var add = num .splice(3,1,60,70)///// add and delete element  first value ma hum yeh bta hai value kaha sy add karni hai
+  /// second ma delete karna k and last ma kia add karna hai wo likhta hai 
+// console.log(num)
+
+///find Array
+var num = [10, 20, 30, 40, 50];
+
+
+
+// question no 1
+// let userNum = parseInt(prompt("plaese enter your lucky number"));
+// if (userNum % 2 === 0) {
+//     alert("your number is " +  userNum  +" " +"is an even number")
+// } else {
+//     alert("your number is " +  userNum  +" " +"is an odd number")
+// }
+
+
+/////question n0 02
+// let score = prompt("kindly enter your score 0-100 ");
+
+// if (score >= 0 && score <= 100) {
+// if (  score >= 90) {
+// alert(" your score number is"+" "+score+" "+"won the match")
+// }else if  (score >= 80) {
+//     alert(" your score number is"+" "+score+" "+"seclte your team")
+// }
+// else if  (score >= 70) {
+//     alert(" your score number is"+" "+score+" "+"seclte your team")
+// }
+//  else if  (score >= 60) {
+//     alert(" your score number is"+" "+score+" "+"seclte your team but you are in danger zone")
+// }
+//  else if  (score >= 50) {
+//     alert(" your score number is"+" "+score+" "+" your fail")
+// }
+// else{
+//     alert("your are not selected")
+
+// }
+// }
+ //////qustion no 03
+//  let userage = parseInt(prompt("Please enter your age"));
+
+//  if (userage >= 10 && userage <= 80) {
+//      if (userage < 14) {
+//          alert("You are a child.");
+//      } else if (userage >= 14 && userage < 22) {
+//          alert("You are a teenager.");
+//      } else if (userage >= 22 && userage < 35) {
+//          alert("You are a young adult.");
+//      } else if (userage >= 35 && userage < 55) {
+//          alert("You are a senior.");
+//      } else if (userage >= 55 && userage <= 80) {
+//          alert("You are a pro senior.");
+//      }
+//  } else {
+//      alert("Please enter an age between 10 and 80.");
+//  }
+  //question no 05
+
+// let userage = parseInt(prompt("check you are  eligible to vote"))
+    
+// if (userage >= 18 ) {
+//     alert("you are eigible")
+// }if (userage <= 18) {
+//     alert("you are not eigible")
+// }
+// let hi /// delcalare 
+// console.log(hi);//// undefined
+
+///// question no 08////
+
+// var   pass= prompt ("plesae give me passward start form 1to8")
+// if (pass== "12345678") {
+//    alert(" great! your passward is right")
+// }if (pass== "12345") {
+//   alert(" your passward is right")
+// }else{
+//   alert("please input right number ")
+// } 
+//// Questiion no 09 /////
+// var shopping = prompt("what are the charges for the shoping have you done?")
+
+// if (shopping >= 15000 ) {
+//   alert("you are eligible  to discont  ")
+// }
+// if (shopping <= 15000 ) {
+//   alert("you are not  eligible  to discont  ")
+// }
+//----------------------- loop
+// for loop ma sub sy phly condution hoti 
+// intazition conduction updating value
+for (let index = 0; index <=5; index++) {
+   console.log("kainat fatima");
+   
+}
